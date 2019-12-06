c96ccde537a20596f9fcf2d08638d9834151faa6
aGuegu <weihong.guan@gmail.com>
2019-12-06 15:38:37 +0800

    CLK and tim4 demo

---
+++ stm8s103f3p6mini/GPIO_Toggle_CLK/src/main.c
@@ -0,0 +1,34 @@
+#include "stm8s_conf.h"
+
+// TEST LED between vcc and PB5
+// Oscilloscope on PA3
+
+void Delay(uint16_t t) {
+  while (t--);
+}
+
+void main(void) {
+  GPIO_Init(GPIOA, GPIO_PIN_3, GPIO_MODE_OUT_PP_LOW_SLOW);
+  GPIO_Init(GPIOB, GPIO_PIN_5, GPIO_MODE_OUT_OD_LOW_SLOW);
+
+  CLK_SYSCLKConfig(CLK_PRESCALER_CPUDIV2);
+  CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1);
+
+  GPIO_WriteReverse(GPIOA, GPIO_PIN_3);
+
+  while (1) {
+    GPIO_WriteReverse(GPIOA, GPIO_PIN_3);
+    GPIO_WriteReverse(GPIOB, GPIO_PIN_5);
+
+    Delay(0xffff);
+  }
+}
+
+#ifdef USE_FULL_ASSERT
+
+void assert_failed(u8* file, u32 line) {
+  (void) file;
+  (void) line;
+  while (1);
+}
+#endif
